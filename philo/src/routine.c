bc61b01479aa4f84fdc4ac126e51d24cb29b0f6c
um4ss <sliziard@icloud.com>
2025-03-02 19:57:52 +0100

    even philosophers do not take fork in same order that not even ones

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,19 +6,27 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:32:18 by sliziard          #+#    #+#             */
-/*   Updated: 2025/03/02 19:28:43 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/03/02 19:55:38 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
 
-static void	_eat(t_philo *phi)
+static void	_take_forks(t_philo p, pthread_mutex_t *one, pthread_mutex_t *two)
 {	
-	pthread_mutex_lock(phi->left_fork);
-	philog(*phi, ACT_FORK);
-	pthread_mutex_lock(phi->right_fork);
-	philog(*phi, ACT_FORK);
+	pthread_mutex_lock(one);
+	philog(p, ACT_FORK);
+	pthread_mutex_lock(two);
+	philog(p, ACT_FORK);
+}
+
+static void	_eat(t_philo *phi)
+{
+	if (phi->id % 2)
+		_take_forks(*phi, phi->left_fork, phi->right_fork);
+	else
+		_take_forks(*phi, phi->right_fork, phi->left_fork);
 	usleep(phi->data->time_to_eat * 1000);
 	phi->meals_eaten++;
 	phi->last_meal_time = date_now();
