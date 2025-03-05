ee1791725da79a769be1166ef06421d66e7a6b0c
um4ss <sliziard@icloud.com>
2025-03-05 11:43:48 +0100

    check the stack'ed size and define ALLOC_MODE if not fit, to use heap instead

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:08:26 by sliziard          #+#    #+#             */
-/*   Updated: 2025/03/04 22:09:16 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/03/05 11:40:19 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,27 @@
 # include <pthread.h>
 # include <stdbool.h>
 
-# ifndef PHILO_MAX
-#  define PHILO_MAX 200
+/**
+ * * Whe have `data` that countains n-count of:
+ * ?-	t_philo (32 | 48)
+ * ?-	t_mutex (40 | 56)
+ * 
+ * The stack is allocated (generaly) with 8 Mo (8 388 608 bytes)
+ * ? Arch 32-bytes
+ * 105 + count * (32 + 40) bytes
+ * * = 116510 PHILO_MAX
+ * ? Arch 64-bytes
+ * 153 + count * (48 + 56) bytes
+ * * = 80643 PHILO_MAX
+ */
+# ifndef ALLOC_MODE
+#  ifndef PHILO_MAX
+#   define PHILO_MAX 200
+#  else
+#   if PHILO_MAX > 80000
+#    define ALLOC_MODE
+#   endif
+#  endif
 # endif
 
 # define USLEEP_INTERVAL 500
@@ -28,6 +47,7 @@
 [number_of_times_each_philosopher_must_eat]"
 # define ERR_INVALID_ARG "Error: Wrong arguments, each one should fit in an \
 unsigned int (except the last one [optional one] that need to fit in a int)\n"
+# define ERR_ALLOC "Error: Allocation failed\n"
 
 typedef enum e_paction
 {
@@ -40,6 +60,17 @@ typedef enum e_paction
 
 typedef pthread_mutex_t	t_mutex;
 
+/**
+ * * Philosopher structure size
+ * id:				int (2 | 4)
+ * meals_eaten:		int (2 | 4)
+ * last_meal_time:	long (8)
+ * thread:			pthread_t (8)
+ * left_fork:		ptr (4 | 8)
+ * right_fork:		ptr (4 | 8)
+ * data:			ptr (4 | 8)
+ * * = 32 | 48 bytes
+ */
 typedef struct s_philo
 {
 	int				id;
@@ -52,6 +83,17 @@ typedef struct s_philo
 	struct s_data	*data;
 }	t_philo;
 
+/**
+ * * Data structure size
+ * ? One mutex ~= 40 | 56 bytes
+ * unsigned int:	(2 | 4) * 3
+ * int:				(2 | 4) * 1
+ * __sim_end:		(1)
+ * start_time:		(8)
+ * t_mutex:			(40 | 56) * 2
+ * ptr:				(4 | 8) * 2
+ * * = 105 | 153 bytes 
+ */
 typedef struct s_data
 {
 	unsigned int	count;
@@ -60,7 +102,7 @@ typedef struct s_data
 	unsigned int	time_to_sleep;
 	int				must_eat_count;
 	long			start_time;
-	bool			__sim_end;
+	int				__sim_end : 1;
 	t_mutex			end_mutex;
 	t_mutex			print_mutex;
 	t_mutex			*forks;
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,12 +6,16 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:08:29 by sliziard          #+#    #+#             */
-/*   Updated: 2025/03/04 22:01:52 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/03/05 11:39:59 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
+
+#ifdef ALLOC_MODE
+# include <stdlib.h>
+#endif
 
 static void	_check_end(t_data *data)
 {
@@ -19,8 +23,8 @@ static void	_check_end(t_data *data)
 	bool	have_all_eat;
 	long	lst_meal;
 
-	have_all_eat = data->must_eat_count != -1;
 	i = 0;
+	have_all_eat = data->must_eat_count != -1;
 	while (i < data->count)
 	{
 		pthread_mutex_lock(&data->philos[i].meal_mutex);
@@ -30,8 +34,8 @@ static void	_check_end(t_data *data)
 		pthread_mutex_unlock(&data->philos[i].meal_mutex);
 		if (date_now() - lst_meal >= data->time_to_die)
 		{
-			philog(data->philos[i], ACT_DIE);
 			set_sim_end(data, true);
+			philog(data->philos[i], ACT_DIE);
 			return ;
 		}
 		i++;
@@ -71,7 +75,15 @@ static void	_destroy_data(t_data *d_ptr)
 	}
 	pthread_mutex_destroy(&d_ptr->print_mutex);
 	pthread_mutex_destroy(&d_ptr->end_mutex);
+	#ifdef ALLOC_MODE
+
+	free(d_ptr->philos);
+	free(d_ptr->forks);
+	#endif
+
 }
+
+#ifndef ALLOC_MODE
 
 int	main(int argc, char const *argv[])
 {
@@ -92,3 +104,36 @@ int	main(int argc, char const *argv[])
 	_destroy_data(&data);
 	return (0);
 }
+
+#else
+
+int	main(int argc, char const *argv[])
+{
+	t_data		data;
+	t_philo		*philos;
+	t_mutex		*forks;
+	pthread_t	monitor;
+
+	if (argc < 5 || argc > 6)
+		return (write(2, ERR_ARG_NB, 160), 1);
+	if (init_data(&data, argc - 1, argv + 1))
+		return (write(2, ERR_INVALID_ARG, 126), 1);
+	philos = malloc(count * sizeof(t_philo));
+	if (!philos)
+		return (write(2, ERR_ALLOC, 25), 1);
+	forks = malloc(count * sizeof(t_mutex));
+	if (!fork)
+	{
+		free(philos);
+		write(2, ERR_ALLOC, 25);
+		return (1);
+	}
+	init_philo_and_forks(&data, philos, forks);
+	pthread_create(&monitor, NULL, &monitoring, (void *)&data);
+	pthread_join(monitor, NULL);
+	while (data.count)
+		pthread_join(data.philos[--data.count].thread, NULL);
+	return (_destroy_data(&data), 0);
+}
+
+#endif
