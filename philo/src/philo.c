d67b1ecbd5bf26bfcdfd4d7c8b6badb890071d92
um4ss <sliziard@icloud.com>
2025-03-15 15:27:01 +0100

    fix: manage when monitoring thread failed to be created to

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:08:29 by sliziard          #+#    #+#             */
-/*   Updated: 2025/03/15 13:18:08 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/03/15 15:26:19 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,29 @@ static inline void	_destroy_data(t_data *d_ptr)
 	}
 }
 
+static inline int16_t	_launch_sim(t_data *dptr)
+{
+	size_t		i;
+	pthread_t	monitor;
+	int16_t		exit;
+
+	if (dptr->must_eat_count == -1)
+		return (0);
+	exit = 0;
+	if (pthread_create(&monitor, NULL, &meal_monitor, dptr))
+	{
+		set_shared(&dptr->sim_state, SH_SET, false);
+		pthread_join(monitor, NULL);
+		exit = 1;
+	}
+	else
+		pthread_detach(&monitor);
+	i = 0;
+	while (i < dptr->count)
+		pthread_join(dptr->philos[i++].thread, NULL);
+	return (exit);
+}
+
 #ifndef ALLOC_MODE
 
 int	main(int argc, char const *argv[])
@@ -65,28 +88,20 @@ int	main(int argc, char const *argv[])
 	t_data		data;
 	t_philo		philos[PHI_MAX + 1];
 	t_mutex		forks[PHI_MAX + 1];
-	pthread_t	monitor;
-	size_t		i;
+	int16_t		exit_code;
 
 	if (argc < 5 || argc > 6)
 		return (write(2, ERR_ARG_NB, 160), 1);
-	i = (size_t)init_data(&data, argc - 1, argv + 1);
-	if (i == 2)
+	exit_code = init_data(&data, argc - 1, argv + 1);
+	if (exit_code == 2)
 		return (write(2, ERR_PHI_MAX_TOHIGH, 66), 1);
-	else if (i)
+	else if (exit_code)
 		return (write(2, ERR_INVALID_ARG, 104), 1);
 	if (init_philo_and_forks(&data, philos, forks))
 		return (1);
-	if (data.must_eat_count != -1)
-	{
-		pthread_create(&monitor, NULL, &meal_monitor, &data);
-		pthread_detach(monitor);
-	}
-	i = 0;
-	while (i < data.count)
-		pthread_join(data.philos[i++].thread, NULL);
+	exit_code = _launch_sim(&data);
 	_destroy_data(&data);
-	return (0);
+	return (exit_code);
 }
 
 #else
@@ -96,8 +111,7 @@ int	main(int argc, char const *argv[])
 	t_data		data;
 	t_philo		*philos;
 	t_mutex		*forks;
-	pthread_t	monitor;
-	size_t		i;
+	int16_t		exit_code;
 
 	if (argc < 5 || argc > 6)
 		return (write(2, ERR_ARG_NB, 160), 1);
@@ -109,13 +123,8 @@ int	main(int argc, char const *argv[])
 		return (free(philos), free(forks), 1);
 	if (init_philo_and_forks(&data, philos, forks))
 		return (free(philos), free(forks), 1);
-	if (data.must_eat_count != -1)
-		(pthread_create(&monitor, NULL, &monitoring, &data), \
-			pthread_join(monitor, NULL));
-	i = 0;
-	while (i < data.count)
-		pthread_join(data.philos[i++].thread, NULL);
-	return (_destroy_data(&data), 0);
+	exit_code = _launch_sim(&data);
+	return (_destroy_data(&data), exit_code);
 }
 
 #endif
