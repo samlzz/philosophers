8325ad04410f8d56dfd3f2cdfd2bf113846db90c
um4ss <sliziard@icloud.com>
2025-03-27 12:59:15 +0100

    fix: check sim_state when we hold print mutex, not before && opti philo death

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:32:18 by sliziard          #+#    #+#             */
-/*   Updated: 2025/03/26 16:08:15 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/03/27 12:49:03 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ static inline bool	_take_forks(t_philo *phi)
 		two = phi->right_fork);
 	pthread_mutex_lock(one);
 	if (one == two)
-	{
-		philog(*phi, ACT_FORK, MX_BOTH);
-		ft_usleep(phi->next_meal_time - date_now());
-		pthread_mutex_unlock(one);
-		return (1);
-	}
+		return (philog(*phi, ACT_FORK, MX_BOTH), \
+				ft_usleep(phi->next_meal_time - date_now()), \
+				pthread_mutex_unlock(one), 1);
 	pthread_mutex_lock(two);
-	if (date_now() >= phi->next_meal_time)
+	pthread_mutex_lock(&phi->data->print_mutex);
+	if (date_now() >= phi->next_meal_time || \
+			!get_shared(&phi->data->sim_state))
 		return (pthread_mutex_unlock(one), \
-				pthread_mutex_unlock(two), 1);
-	philog(*phi, ACT_FORK, MX_LOCK);
+				pthread_mutex_unlock(two), \
+				pthread_mutex_unlock(&phi->data->print_mutex), 1);
+	philog(*phi, ACT_FORK, MX_NONE);
 	philog(*phi, ACT_FORK, MX_NONE);
 	philog(*phi, ACT_EAT, MX_UNLOCK);
 	return (0);
@@ -51,21 +51,21 @@ static bool	_eat(t_philo *phi)
 	now = date_now() - phi->data->start_time;
 	next_die = phi->next_meal_time - phi->data->start_time;
 	if (now + phi->data->time_to_eat >= next_die)
-		ft_usleep(next_die - now);
+			ft_usleep(next_die - now);
 	else
 		ft_usleep(phi->data->time_to_eat);
 	pthread_mutex_unlock(phi->left_fork);
 	pthread_mutex_unlock(phi->right_fork);
 	if (now + phi->data->time_to_eat >= next_die)
 		return (1);
+	phi->last_meal_time = date_now();
+	phi->next_meal_time = phi->last_meal_time + phi->data->time_to_die;
 	phi->meals_eaten++;
 	if (phi->data->must_eat_count != -1 && \
 		phi->meals_eaten == phi->data->must_eat_count)
 	{
 		set_shared(&phi->data->sated, SH_INCREMENT, 1);
 	}
-	phi->last_meal_time = date_now();
-	phi->next_meal_time = phi->last_meal_time + phi->data->time_to_die;
 	return (0);
 }
 
@@ -73,13 +73,14 @@ static bool	_sleep(t_philo *phi)
 {
 	uint64_t	curr_time;
 
-	curr_time = date_now();
+	pthread_mutex_lock(&phi->data->print_mutex);
 	if (!get_shared(&phi->data->sim_state))
-		return (0);
-	philog(*phi, ACT_SLEEP, MX_BOTH);
+		return (pthread_mutex_unlock(&phi->data->print_mutex), 1);
+	philog(*phi, ACT_SLEEP, MX_UNLOCK);
+	curr_time = date_now();
 	if (curr_time + phi->data->time_to_sleep >= phi->next_meal_time)
 	{
-		ft_usleep(phi->next_meal_time - date_now());
+		ft_usleep(phi->next_meal_time - curr_time);
 		return (1);
 	}
 	ft_usleep(phi->data->time_to_sleep);
@@ -88,8 +89,10 @@ static bool	_sleep(t_philo *phi)
 
 static bool	_think(t_philo *phi)
 {
-	if (get_shared(&phi->data->sim_state))
-		philog(*phi, ACT_THINK, MX_BOTH);
+	pthread_mutex_lock(&phi->data->print_mutex);
+	if (!get_shared(&phi->data->sim_state))
+		return (pthread_mutex_unlock(&phi->data->print_mutex), 1);
+	philog(*phi, ACT_THINK, MX_UNLOCK);
 	if (phi->data->count % 2)
 		ft_usleep((phi->next_meal_time - date_now()) / 3);
 	return (0);
@@ -106,17 +109,19 @@ void	*philo_life(void *param)
 		return (NULL);
 	if (phi->id % 2 == 0)
 		ft_usleep(3);
-	else if ((phi->id / 2) % 2 == 0)
-		ft_usleep(1);
 	phi->last_meal_time = phi->data->start_time;
 	phi->next_meal_time = phi->last_meal_time + phi->data->time_to_die;
 	while (get_shared(&phi->data->sim_state))
 	{
 		if (_eat(phi) || _sleep(phi) || _think(phi))
 		{
-			if (get_shared(&phi->data->sim_state))
-				(philog(*phi, ACT_DIE, MX_BOTH), \
-					set_shared(&phi->data->sim_state, SH_SET, false));
+			pthread_mutex_lock(&phi->data->sim_state.mtx);
+			if (phi->data->sim_state.__val)
+			{
+				phi->data->sim_state.__val = false;
+				philog(*phi, ACT_DIE, MX_BOTH);
+			}
+			pthread_mutex_unlock(&phi->data->sim_state.mtx);
 			break ;
 		}
 	}
